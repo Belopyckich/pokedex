6637c52836279e745b353d59e36a6cbc754c7087
belopyckich <belopyckich@gmail.com>
2021-12-14 07:02:33 +0300

    test navbar animation for mobile

--- src/components/NavBar/MobileNavigation.tsx
+++ src/components/NavBar/MobileNavigation.tsx
@@ -1,30 +1,59 @@
 import NavLinks from './NavLinks';
 import style from "./Navbar.module.css";
-import "./Navbar.module.css";
 import hamburgerOpen from "../../images/NavBar/hamburgerOpen.svg";
 import hamburgerClose from "../../images/NavBar/hamburgerClose.svg";
 import back from "../../images/NavBar/leftArrow.svg";
 import { useHistory } from 'react-router-dom';
-import { Transition } from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
+import { useState } from 'react';
 
 interface MobileNavigationProps {
     isOpen: boolean,
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
-const MobileNavigation : React.FC<MobileNavigationProps> = ({isOpen, setIsOpen}) => {
+const MobileNavigation: React.FC<MobileNavigationProps> = ({ isOpen, setIsOpen }) => {
     const history = useHistory();
 
     return (
         <div className={style.mobileNavigation}>
-            <img src={isOpen ? hamburgerClose : hamburgerOpen} alt="hamburger" className={style.hamburger} onClick={() => setIsOpen(!isOpen)}/>
+            <CSSTransition
+                in={isOpen}
+                timeout={1000}
+                classNames={{
+                    enterActive: style["hamburgerEnterActive"],
+                    exitActive: style["hamburgerExitActive"],
+                }}
+            >
+                <img 
+                src={isOpen ? hamburgerClose : hamburgerOpen} 
+                alt="hamburger"
+                className={style.hamburger}
+                onClick={() => setIsOpen(!isOpen)}
+                />
+            </CSSTransition>
+
             <img
-                style={{display: 'flex'}}
+                style={{ display: 'flex' }}
                 src={back}
                 className={style.back}
                 alt="back"
                 onClick={() => history.goBack()}
             />
-            {isOpen &&<NavLinks className={style.navbarWrapper}/>}
+
+            <CSSTransition
+                in={isOpen}
+                timeout={1000}
+                mountOnEnter
+                unmountOnExit
+                classNames={{
+                    enterActive: style["navbarWrapperEnterActive"],
+                    enterDone: style["navbarWrapperEnterDone"],
+                    exitActive: style["navbarWrapperExitActive"],
+                    exitDone: style["navbarWrapperExitDone"]
+                }}
+            >
+                <NavLinks className={style.navbarWrapper} />
+            </CSSTransition>
         </div>
     );
 };
